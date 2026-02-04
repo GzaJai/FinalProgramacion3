47651f40255e9ced499fcebdcb23e94cd249e65a
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-04 17:14:39 -0300

    account page
    Added account page, if user is not authenticated, redirect to login page

---
+++ js/account.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", checkAuth);
+
+function checkAuth() {
+  const token = sessionStorage.getItem("access_token");
+
+  if (!token) {
+    redirectToLogin();
+    return;
+  }
+
+  fetch("http://localhost:8000/auth/me", {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Token inválido");
+      }
+      return res.json();
+    })
+    .then(user => {
+      renderAccount(user);
+    })
+    .catch(() => {
+      localStorage.removeItem("access_token");
+      redirectToLogin();
+    });
+}
+
+function redirectToLogin() {
+  window.location.href = "login.html";
+}
